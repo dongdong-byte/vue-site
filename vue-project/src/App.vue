f5c664736c19ff6348eae3b8488be0e0559eedb4
dongdong-byte <kimeee34@gmail.com>
2025-08-26 15:06:57 +0900

    중요
    ㅇ

---
+++ vue-project/src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import Ex1_bind from './components/Ex1_bind.vue';
+import Ex2_if from './components/Ex2_if.vue';
+import Ex3_show from './components/Ex3_show.vue';
+import Ex4_for from './components/Ex4_for.vue';
+import { ref } from 'vue';
+const activeComponent = ref('Ex1_bind');
+const pages = {
+  Ex1_bind,
+  Ex2_if,
+  Ex3_show,
+  Ex4_for
+};
+</script>
+
+<template>
+  <!-- <Ex1_bind /> -->
+  <!-- <Ex2_if />
+  <Ex3_show />
+  <Ex4_for /> -->
+  <button @click="activeComponent = 'Ex1_bind'">bind_바인드</button>
+  <button @click="activeComponent = 'Ex2_if'">if_이프</button>
+  <button @click="activeComponent = 'Ex3_show'">Show_쇼우</button>
+  <button @click="activeComponent = 'Ex4_for'">for_포</button>
+
+  <div id="comp_view">
+    <keep-alive>
+      <component :is="pages[activeComponent]"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<style scoped>
+button{
+  margin: 5px;
+  filter: drop-shadow(0 0 3px rgba(165, 13, 13, 0.5));
+}
+
+#comp_view {
+  border: 1px solid orange;
+  margin-top: 20px;
+  border-radius: 10px;
+  filter: drop-shadow(1 1 1px rgba(11, 101, 204, 0.5));
+
+}
+</style>
---
+++ vue-project/src/components/Ex4_for.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+ddddsdf
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
